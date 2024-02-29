7381042c5c60fc11b523319d26a424a7fc8631af
Silvana Alves <silvanarodriguesalvess@gmail.com>
2024-02-29 08:14:16 -0300

    Initial Commit

---
+++ CapoEduca/CapoEducaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapoEducaApp.swift
+//  CapoEduca
+//
+//  Created by Silvana Rodrigues Alves on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CapoEducaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
